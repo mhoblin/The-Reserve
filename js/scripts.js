211ffdce9cead00f4b4272567a88c603e3d732ec
mhoblin <monicahoblin@eagles.ewu.edu>
2015-04-17 17:30:07 -0700

    BETTERRRR!

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,5 @@ $(document).ready(function(){
   
   // Your Scripts
 
-  $(window).resize(function(){
-	if ($(window).width() <= 480){
-		$('div#info').removeClass('twelve wide');
-		$('div#info').addClass('sixteen wide')}
-
-	else {$('div#info').removeClass('sixteen wide');
-		$('div#info').addClass('twelve wide')}
-});
 
 });
